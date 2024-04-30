4b5163f68b42e5f6e3e95ec4fed5fcca5a6151a3
Marx Alonso <marxchip99@gmail.com>
2024-04-30 11:48:30 -0500

    Creando index en la carpeta de Home y Locales

---
+++ Aventura-Gym-Laravel/resources/views/Home/index.blade.php
@@ -0,0 +1,24 @@
+<section id="hero-856" class="container-fluid text-white py-5">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <div class="cs-content text-center">
+                <span class="cs-topper">AVENTURA GYM</span>
+                <h1 class="cs-title">¡Transforma tu cuerpo y tu vida!</h1>
+                <p class="cs-text">
+                    Descubre tu potencial y alcanza tus metas de fitness en Aventura Gym. Con nuestro equipo de entrenadores expertos, instalaciones de vanguardia y una comunidad de apoyo, te ayudaremos a alcanzar tu mejor versión.
+                </p>
+                <a href="contacto" class="btn btn-primary">¡Únete a nosotros!</a>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-6">
+            <div class="cs-picture">
+                <img src="{{ asset('images/fondo-gym.webp')}}" alt="Fondo" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <svg class="cs-wave" width="1920" height="179" viewBox="0 0 1920 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M1920 179V91.3463C1835.33 91.3463 1715.47 76.615 1549.2 32.9521C1299.48 -32.3214 1132.77 12.1006 947.32 61.5167C810.762 97.9044 664.042 137 466.533 137C331.607 137 256.468 123.447 188.082 111.113C130.974 100.812 78.5746 91.3609 0 91.3609V179H1920Z" fill="white"/>
+    </svg>
+</section>
+<br>
